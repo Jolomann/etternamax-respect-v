cd77064c0371e3a2927a8621f177957d2ee9eb8b
External69 <hotmailsucks1234athotmail.com@gmail.com>
2019-12-29 16:05:15 -0500

    tie up loose ends on menu scroller to fix its mechanics

--- Themes/etternamax respect v/BGAnimations/ScreenTitleMenu decorations/ScrollerImages.lua
+++ Themes/etternamax respect v/BGAnimations/ScreenTitleMenu decorations/ScrollerImages.lua
@@ -2,24 +2,41 @@ local mainImageWidth = 200
 local mainImageHeight = 200
 local transitionSeconds = 0.1
 
+
+local scrollerChoices = THEME:GetMetric("ScreenTitleMenu", "ChoiceNames")
+
+local choices = {}
+local i = 0
+for name in string.gmatch(scrollerChoices, "%a+") do
+	i = i + 1
+	choices[name] = i
+end
+
+local str = ''
+
 local lastSelected = ''
-local transitionEven = false -- wow this variable naming sucks ass find a better name for it
+local leftTransition = true
 local t = Def.ActorFrame{
 	InitCommand = function(self)
 		self:SetUpdateFunction(function()
-			
 			local selected = SCREENMAN:GetTopScreen()["selected"]
 			if lastSelected ~= selected then
 				if lastSelected ~= '' then
-					transitionEven = not transitionEven
-				
+					if choices[selected] - choices[lastSelected] ~= -1 and choices[selected] - choices[lastSelected] <= 1 then --- last = 1, selected = 6
+						leftTransition = true
+					else
+						leftTransition = false
+					end
+					
 					lastSelected = selected
 					
-					MESSAGEMAN:Broadcast("MenuSelectionChanged")
+					self:GetChildren()["ScrollerImage1"]:playcommand("MenuSelectionChanged")
+					self:GetChildren()["ScrollerImage2"]:playcommand("MenuSelectionChanged")
 				else
 					lastSelected = selected
 				end
 			end
+			
 		end)
 	end
 }
@@ -27,10 +44,9 @@ local t = Def.ActorFrame{
 
 -- the sprites below trade jobs on each transition
 -- at any given time, one is occupying the middle of the screen and the other is hiding off the edge (to be repositioned right or left depending on which direction scroll)
--- changing whether they Load graphics before or after the tween i think is also necessary (queuecommand) ((don't quote me on this))
 
 t[#t+1] = Def.Sprite{
-	--Name = "fffffff" -- come up with an appropriate name for this and the other one
+	Name = "ScrollerImage1",
 	
 	InitCommand = function(self)
 		self:Load(THEME:GetPathG("", "MenuItems/GameStart"))
@@ -40,40 +56,62 @@ t[#t+1] = Def.Sprite{
 		self:xy(SCREEN_CENTER_X, SCREEN_CENTER_Y)
 	end,
 	
-	MenuSelectionChangedMessageCommand  = function(self)
-		self:Load(THEME:GetPathG("", "MenuItems/" .. lastSelected))
+	MenuSelectionChangedCommand  = function(self)
+		self:finishtweening()
 		
-		self:stoptweening()
-		self:linear(transitionSeconds)
-		if transitionEven then
-			self:x(0 - mainImageWidth/2)
+		local xDest
+		local xStart
+		if leftTransition then
+			xDest = 0 - mainImageWidth/2 -- goes left
+			xStart = SCREEN_WIDTH + mainImageWidth/2 -- or starts right
 		else
-			self:x(SCREEN_CENTER_X)
+			xDest = SCREEN_WIDTH + mainImageWidth/2 -- goes right
+			xStart = 0 - mainImageWidth/2 -- or starts left
 		end
+		
+		if self:GetX() ~= SCREEN_CENTER_X then
+			self:Load(THEME:GetPathG("", "MenuItems/" .. lastSelected))
+			self:x(xStart)
+			xDest = SCREEN_CENTER_X
+		end
+		
+		self:linear(transitionSeconds) -- decelerate
+		self:x(xDest)
 	end
 }
 
 t[#t+1] = Def.Sprite{
-	--Name = "?????????" -- come up with an appropriate name for this and the other one
+	Name = "ScrollerImage2",
 	
 	InitCommand = function(self)
 		self:Load(THEME:GetPathG("", "MenuItems/GameStart"))
 		
 		self:stretchto(0,0, mainImageWidth, mainImageHeight) -- stop being lazy and use set size or wahtever 
 		self:halign(0.5):valign(0.5)
-		self:xy(SCREEN_WIDTH + mainImageWidth, SCREEN_CENTER_Y)
+		self:xy(SCREEN_WIDTH + mainImageWidth/2, SCREEN_CENTER_Y)
 	end,
 	
-	MenuSelectionChangedMessageCommand  = function(self)
-		self:Load(THEME:GetPathG("", "MenuItems/" .. lastSelected))
+	MenuSelectionChangedCommand  = function(self)
+		self:finishtweening()
 		
-		self:stoptweening()
-		self:linear(transitionSeconds)
-		if not transitionEven then
-			self:x(0 - mainImageWidth/2)
+		local xDest
+		local xStart
+		if leftTransition then
+			xDest = 0 - mainImageWidth/2 -- goes left
+			xStart = SCREEN_WIDTH + mainImageWidth/2 -- or starts right
 		else
-			self:x(SCREEN_CENTER_X)
+			xDest = SCREEN_WIDTH + mainImageWidth/2 -- goes right
+			xStart = 0 - mainImageWidth/2 -- or starts left
 		end
+		
+		if self:GetX() ~= SCREEN_CENTER_X then
+			self:Load(THEME:GetPathG("", "MenuItems/" .. lastSelected))
+			self:x(xStart)
+			xDest = SCREEN_CENTER_X
+		end
+		
+		self:linear(transitionSeconds) -- decelerate
+		self:x(xDest)
 	end
 }
 
